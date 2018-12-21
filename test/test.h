277b56dd8f6979198314a9568db58c284b0a399c
Nat! <nat@mulle-kybernetik.com>
2018-12-21 23:35:39 +0100

    add missing files

---
+++ test/test.h
@@ -0,0 +1,303 @@
+// test.h
+// Common definitions for trivial test harness
+
+
+#ifndef TEST_H
+#define TEST_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h> // questionable
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/param.h>
+
+#include <mulle-objc-runtime/mulle-objc-runtime.h>
+#include <MulleObjC/mulle-objc.h>
+#include <objc/objc-runtime.h>
+
+
+#define OBJC_ROOT_CLASS
+#define OBJC_INLINE inline
+#define __unused
+
+
+static OBJC_INLINE void *objc_collectableZone(void) { return nil; }
+
+
+// Configuration macros
+
+#if !__LP64__ || TARGET_OS_WIN32 || __OBJC_GC__ || TARGET_IPHONE_SIMULATOR
+#   define SUPPORT_NONPOINTER_ISA 0
+#elif __x86_64__
+#   define SUPPORT_NONPOINTER_ISA 1
+#elif __arm64__
+#   define SUPPORT_NONPOINTER_ISA 1
+#else
+#   error unknown architecture
+#endif
+
+
+// Test output
+
+static inline void succeed(const char *name)  __attribute__((noreturn));
+static inline void succeed(const char *name)
+{
+    if (name) {
+        char path[MAXPATHLEN+1];
+        strcpy(path, name);
+        fprintf(stderr, "OK: %s\n", basename(path));
+    } else {
+        fprintf(stderr, "OK\n");
+    }
+    exit(0);
+}
+
+static inline void fail(const char *msg, ...)   __attribute__((noreturn));
+static inline void fail(const char *msg, ...)
+{
+    if (msg) {
+        char *msg2;
+        asprintf(&msg2, "BAD: %s\n", msg);
+        va_list v;
+        va_start(v, msg);
+        vfprintf(stderr, msg2, v);
+        va_end(v);
+        free(msg2);
+    } else {
+        fprintf(stderr, "BAD\n");
+    }
+    exit(1);
+}
+
+#define testassert(cond) \
+    ((void) (((cond) != 0) ? (void)0 : __testassert(#cond, __FILE__, __LINE__)))
+#define __testassert(cond, file, line) \
+    (fail("failed assertion '%s' at %s:%u", cond, __FILE__, __LINE__))
+
+/* time-sensitive assertion, disabled under valgrind */
+#define timecheck(name, time, fast, slow)                                    \
+    if (getenv("VALGRIND") && 0 != strcmp(getenv("VALGRIND"), "NO")) {  \
+        /* valgrind; do nothing */                                      \
+    } else if (time > slow) {                                           \
+        fprintf(stderr, "SLOW: %s %llu, expected %llu..%llu\n",         \
+                name, (uint64_t)(time), (uint64_t)(fast), (uint64_t)(slow)); \
+    } else if (time < fast) {                                           \
+        fprintf(stderr, "FAST: %s %llu, expected %llu..%llu\n",         \
+                name, (uint64_t)(time), (uint64_t)(fast), (uint64_t)(slow)); \
+    } else {                                                            \
+        testprintf("time: %s %llu, expected %llu..%llu\n",              \
+                   name, (uint64_t)(time), (uint64_t)(fast), (uint64_t)(slow)); \
+    }
+
+
+static inline void testprintf(const char *msg, ...)
+{
+    static int verbose = -1;
+    if (verbose < 0) verbose = atoi(getenv("VERBOSE") ?: "0");
+
+    // VERBOSE=1 prints test harness info only
+    if (msg  &&  verbose >= 2) {
+        char *msg2;
+        asprintf(&msg2, "VERBOSE: %s", msg);
+        va_list v;
+        va_start(v, msg);
+        vfprintf(stderr, msg2, v);
+        va_end(v);
+        free(msg2);
+    }
+}
+
+// complain to output, but don't fail the test
+// Use when warning that some test is being temporarily skipped
+// because of something like a compiler bug.
+static inline void testwarn(const char *msg, ...)
+{
+    if (msg) {
+        char *msg2;
+        asprintf(&msg2, "WARN: %s\n", msg);
+        va_list v;
+        va_start(v, msg);
+        vfprintf(stderr, msg2, v);
+        va_end(v);
+        free(msg2);
+    }
+}
+
+static inline void testnoop() { }
+
+#define testexc()
+#define testcollect()
+
+
+// Synchronously run test code on another thread.
+// This can help force GC to kill objects promptly, which some tests depend on.
+
+// The block object is unsafe_unretained because we must not allow
+// ARC to retain them in non-Foundation tests
+static void (*testcodehack)( void);
+static inline void *_testthread(void *arg __unused)
+{
+    //objc_registerThreadWithCollector();
+    testcodehack();
+    return NULL;
+}
+static inline void testonthread( void (*code) (void))
+{
+    pthread_t th;
+    testcodehack = code;  // force GC not-thread-local, avoid ARC void* casts
+    pthread_create(&th, NULL, _testthread, NULL);
+    pthread_join(th, NULL);
+}
+
+
+static inline BOOL is_guardmalloc(void)
+{
+    const char *env = getenv("GUARDMALLOC");
+    return (env  &&  0 == strcmp(env, "YES"));
+}
+
+
+/* Memory management compatibility macros */
+
+static id self_fn(id x) __attribute__((used));
+static id self_fn(id x) { return x; }
+
+#if __has_feature(objc_arc)
+    // ARC
+#   define RELEASE_VAR(x)            x = nil
+#   define WEAK_STORE(dst, val)      (dst = (val))
+#   define WEAK_LOAD(src)            (src)
+#   define SUPER_DEALLOC()
+#   define RETAIN(x)                 (self_fn(x))
+#   define RELEASE_VALUE(x)          ((void)self_fn(x))
+#   define AUTORELEASE(x)            (self_fn(x))
+
+#elif defined(__OBJC_GC__)
+    // GC
+#   define RELEASE_VAR(x)            x = nil
+#   define WEAK_STORE(dst, val)      (dst = (val))
+#   define WEAK_LOAD(src)            (src)
+#   define SUPER_DEALLOC()           [super dealloc]
+#   define RETAIN(x)                 [x self]
+#   define RELEASE_VALUE(x)          (void)[x self]
+#   define AUTORELEASE(x)            [x self]
+
+#else
+    // MRC
+#   define RELEASE_VAR(x)            do { [x release]; x = nil; } while (0)
+#   define WEAK_STORE(dst, val)      objc_storeWeak((id *)&dst, val)
+#   define WEAK_LOAD(src)            objc_loadWeak((id *)&src)
+#   define SUPER_DEALLOC()           [super dealloc]
+#   define RETAIN(x)                 [x retain]
+#   define RELEASE_VALUE(x)          [x release]
+#   define AUTORELEASE(x)            [x autorelease]
+#endif
+
+/* gcc compatibility macros */
+/* <rdar://problem/9412038> @autoreleasepool should generate objc_autoreleasePoolPush/Pop on 10.7/5.0 */
+//#if !defined(__clang__)
+#   define PUSH_POOL { void *pool = objc_autoreleasePoolPush();
+#   define POP_POOL objc_autoreleasePoolPop(pool); }
+//#else
+//#   define PUSH_POOL @autoreleasepool
+//#   define POP_POOL
+//#endif
+
+#if __OBJC__
+
+/* General purpose root class */
+
+OBJC_ROOT_CLASS
+@interface TestRoot {
+// @public
+//    Class isa;  // not in mulle-objc
+}
+
++(void) load;
++(void) initialize;
+
+-(id) self;
+-(Class) class;
+-(Class) superclass;
+
++(id) new;
++(id) alloc;
++(id) allocWithZone:(void*)zone;
+-(id) copy;
+-(id) mutableCopy;
+-(id) init;
+-(void) dealloc;
+-(void) finalize;
+@end
+@interface TestRoot (RR)
+-(id) retain;
+-(oneway void) release;
+-(id) autorelease;
+-(unsigned long) retainCount;
+-(id) copyWithZone:(void *)zone;
+-(id) mutableCopyWithZone:(void*)zone;
+@end
+
+// incremented for each call of TestRoot's methods
+extern mulle_atomic_pointer_t TestRootLoad;
+extern mulle_atomic_pointer_t TestRootInitialize;
+extern mulle_atomic_pointer_t TestRootAlloc;
+extern mulle_atomic_pointer_t TestRootAllocWithZone;
+extern mulle_atomic_pointer_t TestRootCopy;
+extern mulle_atomic_pointer_t TestRootCopyWithZone;
+extern mulle_atomic_pointer_t TestRootMutableCopy;
+extern mulle_atomic_pointer_t TestRootMutableCopyWithZone;
+extern mulle_atomic_pointer_t TestRootInit;
+extern mulle_atomic_pointer_t TestRootDealloc;
+extern mulle_atomic_pointer_t TestRootFinalize;
+extern mulle_atomic_pointer_t TestRootRetain;
+extern mulle_atomic_pointer_t TestRootRelease;
+extern mulle_atomic_pointer_t TestRootAutorelease;
+extern mulle_atomic_pointer_t TestRootRetainCount;
+extern mulle_atomic_pointer_t TestRootTryRetain;
+extern mulle_atomic_pointer_t TestRootIsDeallocating;
+extern mulle_atomic_pointer_t TestRootPlusRetain;
+extern mulle_atomic_pointer_t TestRootPlusRelease;
+extern mulle_atomic_pointer_t TestRootPlusAutorelease;
+extern mulle_atomic_pointer_t TestRootPlusRetainCount;
+
+#endif
+
+
+// Struct that does not return in registers on any architecture
+
+struct stret {
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+    int i;
+    int j;
+};
+
+static inline BOOL stret_equal(struct stret a, struct stret b)
+{
+    return (a.a == b.a  &&
+            a.b == b.b  &&
+            a.c == b.c  &&
+            a.d == b.d  &&
+            a.e == b.e  &&
+            a.f == b.f  &&
+            a.g == b.g  &&
+            a.h == b.h  &&
+            a.i == b.i  &&
+            a.j == b.j);
+}
+
+static struct stret STRET_RESULT __attribute__((used)) = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+#endif
---
+++ test/testroot.i
@@ -0,0 +1,232 @@
+// testroot.i
+// Implementation of class TestRoot
+// Include this file into your main test file to use it.
+
+#include "test.h"
+
+#include <mulle-thread/mulle-thread.h>
+
+
+mulle_atomic_pointer_t TestRootLoad;
+mulle_atomic_pointer_t TestRootInitialize;
+mulle_atomic_pointer_t TestRootAlloc;
+mulle_atomic_pointer_t TestRootAllocWithZone;
+mulle_atomic_pointer_t TestRootCopy;
+mulle_atomic_pointer_t TestRootCopyWithZone;
+mulle_atomic_pointer_t TestRootMutableCopy;
+mulle_atomic_pointer_t TestRootMutableCopyWithZone;
+mulle_atomic_pointer_t TestRootInit;
+mulle_atomic_pointer_t TestRootDealloc;
+mulle_atomic_pointer_t TestRootFinalize;
+mulle_atomic_pointer_t TestRootRetain;
+mulle_atomic_pointer_t TestRootRelease;
+mulle_atomic_pointer_t TestRootAutorelease;
+mulle_atomic_pointer_t TestRootRetainCount;
+mulle_atomic_pointer_t TestRootTryRetain;
+mulle_atomic_pointer_t TestRootIsDeallocating;
+mulle_atomic_pointer_t TestRootPlusRetain;
+mulle_atomic_pointer_t TestRootPlusRelease;
+mulle_atomic_pointer_t TestRootPlusAutorelease;
+mulle_atomic_pointer_t TestRootPlusRetainCount;
+
+
+@implementation TestRoot
+
+// These all use void* pending rdar://9310005.
+
+static void *
+retain_fn(void *self, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootRetain);
+
+    _mulle_objc_object_inlineretain(self);
+    return( self);
+}
+
+static void
+release_fn(void *self, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootRelease);
+
+    _mulle_objc_object_inlinerelease(self);
+}
+
+static void *
+autorelease_fn(void *self, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootAutorelease);
+    return(self);
+}
+
+static unsigned long
+retaincount_fn(void *self, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootRetainCount);
+    return( (unsigned long) mulle_objc_object_get_retaincount( self));
+}
+
+static void *
+copywithzone_fn(void *self, SEL _cmd __unused, void *zone) {
+    size_t                           size;
+    struct _mulle_objc_class        *cls;
+    struct _mulle_objc_infraclass   *infra;
+
+    _mulle_atomic_pointer_increment(&TestRootCopyWithZone);
+    cls   = _mulle_objc_object_get_isa( self);
+    infra = _mulle_objc_class_as_infraclass( cls);
+    size  = _mulle_objc_infraclass_get_instancesize( infra);
+    return object_copy(self, size);
+}
+
+static void *
+plusretain_fn(void *self __unused, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootPlusRetain);
+    return self;
+}
+
+static void
+plusrelease_fn(void *self __unused, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootPlusRelease);
+}
+
+static void *
+plusautorelease_fn(void *self, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootPlusAutorelease);
+    return self;
+}
+
+static unsigned long
+plusretaincount_fn(void *self __unused, SEL _cmd __unused) {
+    _mulle_atomic_pointer_increment(&TestRootPlusRetainCount);
+    return ULONG_MAX;
+}
+
++(void) load {
+    _mulle_atomic_pointer_increment(&TestRootLoad);
+
+    // install methods that ARR refuses to compile
+    class_addMethod(self, sel_registerName("retain"), (IMP)retain_fn, "");
+    class_addMethod(self, sel_registerName("release"), (IMP)release_fn, "");
+    class_addMethod(self, sel_registerName("autorelease"), (IMP)autorelease_fn, "");
+    class_addMethod(self, sel_registerName("retainCount"), (IMP)retaincount_fn, "");
+    class_addMethod(self, sel_registerName("copyWithZone:"), (IMP)copywithzone_fn, "");
+
+    class_addMethod(object_getClass(self), sel_registerName("retain"), (IMP)plusretain_fn, "");
+    class_addMethod(object_getClass(self), sel_registerName("release"), (IMP)plusrelease_fn, "");
+    class_addMethod(object_getClass(self), sel_registerName("autorelease"), (IMP)plusautorelease_fn, "");
+    class_addMethod(object_getClass(self), sel_registerName("retainCount"), (IMP)plusretaincount_fn, "");
+}
+
+
++(void) initialize {
+    _mulle_atomic_pointer_increment(&TestRootInitialize);
+}
+
+-(id) self {
+    return self;
+}
+
++(Class) class {
+    return self;
+}
+
+-(Class) class {
+    return object_getClass(self);
+}
+
++(Class) superclass {
+    return class_getSuperclass(self);
+}
+
+-(Class) superclass {
+    return class_getSuperclass([self class]);
+}
+
++(id) new {
+    return [[self alloc] init];
+}
+
++(id) alloc {
+    _mulle_atomic_pointer_increment(&TestRootAlloc);
+    return( (id) _mulle_objc_infraclass_alloc_instance( self));
+}
+
++(id) allocWithZone:(void *)zone {
+    _mulle_atomic_pointer_increment(&TestRootAllocWithZone);
+    return( (id) _mulle_objc_infraclass_alloc_instance( self));
+}
+
++(id) copy {
+    return self;
+}
+
++(id) copyWithZone:(void *) __unused zone {
+    return self;
+}
+
+-(id) copy {
+    _mulle_atomic_pointer_increment(&TestRootCopy);
+    return [self copyWithZone:NULL];
+}
+
++(id) mutableCopyWithZone:(void *) __unused zone {
+    fail("+mutableCopyWithZone: called");
+}
+
+-(id) mutableCopy {
+    _mulle_atomic_pointer_increment(&TestRootMutableCopy);
+    return [self mutableCopyWithZone:NULL];
+}
+
+-(id) mutableCopyWithZone:(void *) __unused zone {
+    _mulle_atomic_pointer_increment(&TestRootMutableCopyWithZone);
+    return( (id) _mulle_objc_infraclass_alloc_instance( object_getClass(self)));
+}
+
+-(id) init {
+    _mulle_atomic_pointer_increment(&TestRootInit);
+    return( self);
+}
+
++(void) dealloc {
+    fail("+dealloc called");
+}
+
+-(void) dealloc {
+    _mulle_atomic_pointer_increment(&TestRootDealloc);
+    _mulle_objc_object_free( ( struct _mulle_objc_object *) self);
+}
+
++(void) finalize {
+    fail("+finalize called");
+}
+
+-(void) finalize {
+    _mulle_atomic_pointer_increment(&TestRootFinalize);
+}
+
++(BOOL) _tryRetain {
+    return YES;
+}
+
+-(BOOL) _tryRetain {
+    _mulle_atomic_pointer_increment(&TestRootTryRetain);
+    _mulle_objc_object_increment_retaincount(self);
+    return( YES);
+}
+
++(BOOL) _isDeallocating {
+    return NO;
+}
+
+-(BOOL) _isDeallocating {
+    _mulle_atomic_pointer_increment(&TestRootIsDeallocating);
+    return( NO);
+}
+
+-(BOOL) allowsWeakReference {
+    return ! [self _isDeallocating];
+}
+
+-(BOOL) retainWeakReference {
+    return [self _tryRetain];
+}
+
+
+@end
