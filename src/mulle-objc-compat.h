3a82239a3e1e7cb6d6e54b6df6f8ddd849eb7f59
Nat! <nat@mulle-kybernetik.com>
2018-12-21 22:42:54 +0100

    fix travis

--- src/mulle-objc-compat.h
+++ src/mulle-objc-compat.h
@@ -5,7 +5,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_OBJC_COMPAT_VERSION  ((1 << 20) | (0 << 8) | 0)
+#define MULLE_OBJC_COMPAT_VERSION  ((0 << 20) | (14 << 8) | 0)
 
 #include "runtime.h"
 #include "method.h"
