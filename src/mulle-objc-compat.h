829d4600b103a06806f56654c69c92ae549fcacf
Nat! <nat@mulle-kybernetik.com>
2020-04-18 20:38:42 +0200

    * new mulle-sde project structure

--- src/mulle-objc-compat.h
+++ src/mulle-objc-compat.h
@@ -5,7 +5,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_OBJC_COMPAT_VERSION  ((0 << 20) | (17 << 8) | 0)
+#define MULLE_OBJC_COMPAT_VERSION  ((0 << 20) | (17 << 8) | 1)
 
 #include "runtime.h"
 #include "method.h"
